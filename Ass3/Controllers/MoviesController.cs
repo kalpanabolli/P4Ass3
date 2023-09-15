1317dee417e6ea23a35549792fb9c08a3d35d380
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-09-15 19:14:16 +0530

    first commit

---
+++ Ass3/Controllers/MoviesController.cs
@@ -0,0 +1,62 @@
+﻿using Ass3.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        static List<Movie> movies = new List<Movie>
+        {
+
+            new Movie(){Id=1,MName="LoveToday",}
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> Get()
+        {
+            return movies;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Movie> Get(int id)
+        {
+            var movie = movies.Find(t => t.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return movie;
+        }
+        [HttpPost]
+        public void Post([FromBody] Movie movie)
+        {
+            movie.Id = movies.Count + 1;
+            movies.Add(movie);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Movie updatedMovie)
+        {
+            var movie = movies.Find(t => t.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            movie.MName = updatedMovie.MName;
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var movie = movies.Find(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            movies.Remove(movie);
+            return NoContent();
+        }
+    }
+}
---
+++ Ass3/Model/Movie.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Ass3.Model
+{
+    
+        
+        public class Movie
+        {
+            
+            public int Id { get; set; }
+        
+            public string MName { get; set; }
+            
+        }
+    
+}
